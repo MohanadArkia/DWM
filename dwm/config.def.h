b4ce6d14183393c12c41d7d960b97652e1d0c1de
MohanadArkia <arkia.ar.47@gmail.com>
2024-11-15 16:36:50 +0200

    Edited slstatus and removed current window text

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -65,14 +65,14 @@ static const char *browser[] =  { "brave" };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY|ShiftMask,             XK_h,      focusstack,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_l,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_l,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	/* { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} }, */
+	/* { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} }, */
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
@@ -106,7 +106,6 @@ static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
--- slstatus/config.h
+++ slstatus/config.h
@@ -65,9 +65,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ battery_perc, "BATT: %s% | ",    "BAT1"      },
-    { disk_free, "HDD:%s | ",         "/dev/sda3" },
-    { cpu_perc, "CPU:%s | ",           NULL        },
-    { ram_perc, "RAM:%s | ",           NULL        },
-	{ datetime, "%s",               "%F %T"     },
+	{ battery_perc, "BATT: %s% | ",         "BAT1"      },
+    { disk_free, "HDD: %s | ",              "/dev/sda3" },
+    { cpu_perc, "CPU: %s% | ",              NULL        },
+    { ram_perc, "RAM: %s% | ",              NULL        },
+    { run_command, "Vol: %s | ",        "pamixer --get-volume-human | awk '{print $1}'"        },
+	{ datetime, "%s",                   "%a %b %d %r"     },
 };
